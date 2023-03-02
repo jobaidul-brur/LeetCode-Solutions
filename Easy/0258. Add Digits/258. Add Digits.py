794fa491f8d8ae4843b61fd7f20eadc37a53fd4e
Md. Jobaidul Islam <jobaidul.islam.432@gmail.com>
2023-03-03 01:32:57 +0600

    Create 258. Add Digits.py

---
+++ Easy/0258. Add Digits/258. Add Digits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        num %= 9
+        if num == 0:
+            num = 9
+        return num
