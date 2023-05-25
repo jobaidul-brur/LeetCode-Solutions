af0b54581f77c99aeee57f35d67d2e879120a4d2
Md. Jobaidul Islam <jobaidul.cse.brur@gmail.com>
2023-05-25 23:01:42 +0600

    Create 837. New 21 Game.py

---
+++ Medium/0837. New 21 Game/837. New 21 Game.py
@@ -0,0 +1,14 @@
+class Solution:
+    def new21Game(self, n: int, k: int, maxPts: int) -> float:
+        dp = [0] * (n+1)
+        dp[0] = 1
+        s = 1 if k > 0 else 0
+
+        for i in range(1, n + 1):
+            dp[i] = s/maxPts
+            if i < k:
+                s += dp[i]
+            if 0 <= i-maxPts < k:
+                s -= dp[i-maxPts]
+        
+        return sum(dp[k:])
